7dae97e904d75b9d925a76cf4bd507ed7ec9cbe5
kiri0716 <kirito07160716@gmail.com>
2025-01-13 16:43:22 +0900

    ファイル名

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Card from './Card';
 import { Card as CardType, cardContents } from '../types/CardType';
-import Modal from './modal';
+import Modal from './Modal';
 
 
 interface MemoryGameState {
